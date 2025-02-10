87640faa464d6060ccc99af65b7d23a34e83fbf0
Jan Eckerlein <eckerlein.jan@gmail.com>
2025-02-10 17:38:01 +0100

    chore: update project configuration and remove default collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,26 +1,19 @@
 // storage-adapter-import-placeholder
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
-import sharp from 'sharp'
-
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
   admin: {
-    user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -31,9 +24,20 @@ export default buildConfig({
       url: process.env.DATABASE_URI || '',
     },
   }),
-  sharp,
-  plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+  endpoints: [
+    {
+      path: '/test1/*',
+      method: 'get',
+      handler: async (req) => {
+        return new Response('Hello World')
+      },
+    },
+    {
+      path: '/test2/*capture',
+      method: 'get',
+      handler: async (req) => {
+        return new Response('Hello World')
+      },
+    },
   ],
 })
